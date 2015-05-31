b65d02df5f5784b4597ee0c4f77d83bfee69e35d
Max <IndenML@gmail.com>
2015-05-31 18:12:38 +0200

    Added console logs to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,15 +11,19 @@ public class Main {
 
         /* Load Image */
         ith.readImage("FullSizeTestImage.jpg");
+        System.out.println("Image file successfully loaded");
 
         /* Build two dimensional boolean array of pictures */
         boolean [][]bol = ith.ImageToBinaryArray();
+        System.out.println("Image successfully transfered to boolean array");
 
         /* Translate each 15+1 bit boolean block to one decimal number */
         int[][] finish = ith.BinaryArrayToDecimalArray(bol);
+        System.out.println("Binary array successfully transformed to decimals");
 
         /* Write the decimal Array and the hack instructions to a file*/
         ith.writeDecimalToFile(finish);
+        System.out.println("Assembler file successfully written to file system");
     }
 
 }
